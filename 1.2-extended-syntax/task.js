65a6cbd7d4a139ee20a62454b3ed74a8aa324c19
Anna Nosova <anna.nosova.int@gmail.com>
2020-01-20 13:52:20 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,4 +1,7 @@
 
+'use strict'
+
+//task 1
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -11,9 +14,19 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+    let d = Math.pow(b, 2) - 4 * a * c;
+    let result = [];
+
+    if (d > 0) {
+        result[0] = (-b + Math.sqrt(d)) / (2 * a);
+        result[1] = (-b - Math.sqrt(d)) / (2 * a);
+    } else if (d === 0) {
+        result[0] = -b  / (2 * a); 
+    }
+    return result;
 }
+
+// task 2
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
@@ -22,19 +35,45 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+
+    if (marks.length > 5) {
+        console.log ('Превышено максимальное количество оценок. Будет произведен расчет по первым пяти оценкам')
+        marks.splice(5, marks.length - 5);
+    }
+
+    let result;
+    let marksSum = 0;
+
+    for (let i = 0; i <marks.length; i++) {
+        marksSum += marks[i];
+    }
+
+    result = marksSum / marks.length;
+    return result;
 }
+
+// task 3
 
 function calculateDrinkTask(){
     let name = window.personName.value;
+    console.log(name);
     let dateOfBirthday = new Date(window.dateOfBirthday.value);
     let drink = askDrink(name, dateOfBirthday);
     window.drink.textContent = drink;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let birthYear = dateOfBirthday.getFullYear();
+    let today = new Date();
+    let nowYear = today.getFullYear();
+    let result;
+
+    if ((nowYear - birthYear) >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+
+    console.log(result)
+    return result;
 }
