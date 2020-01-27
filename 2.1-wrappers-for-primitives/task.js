28e7eff42faa31f61bbb228eab4af1afabc5c898
Anna Nosova <anna.nosova.int@gmail.com>
2020-01-27 15:54:01 +0300

    first commit wrappers-for-primitives

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,19 +1,45 @@
+'use strickt'
+
+//task 1
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
-    let date = window.date.value;
-
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let date = new Date (window.date.value);
+    let currentDate = new Date();
+    let monthsAmount = Math.round((Math.abs(date.getTime() - currentDate.getTime())) / (1000 * 3600 * 24 * 30.5));
+    let result = calculateTotalMortgage(percent, contribution, amount, monthsAmount);
     let span = window.mortageResult;
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function calculateTotalMortgage(percent, contribution, amount, monthsAmount) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+
+    let mortgageData = {
+        percent: parseInt(percent),
+        contribution: parseInt(contribution),
+        amount: parseInt(amount),
+        monthsAmount: monthsAmount,
+    }
+
+    for (let key in Object(mortgageData)) {
+        console.log(mortgageData[key])
+        if (isNaN(mortgageData[key]) || mortgageData[key] === undefined) {
+            return console.log(`Параметр ${key} содержит неправильное значение ${mortgageData[key]}`);
+            break;
+        } 
+    }
+
+
+    mortgageData.index = mortgageData.percent / 100 / 12;
+    let monthlyPayment = (mortgageData.amount - mortgageData.contribution) * (mortgageData.index + mortgageData.index / (((1 + mortgageData.index) ** mortgageData.monthsAmount) - 1))
+    let mortgageResult = (monthlyPayment * mortgageData.monthsAmount).toFixed(2);
+    return (mortgageResult);
 }
+
+//task 2
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +49,9 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if(name === '') {
+        return alert('Привет, мир! Меня зовут Аноним');
+    } else {
+        alert(`Привет, мир! Меня зовут ${name}`);
+    }
 }
