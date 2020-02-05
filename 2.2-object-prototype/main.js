c2df3b099642e74995c8df0bda273f7d3e729f3d
Anna Nosova <anna.nosova.int@gmail.com>
2020-02-05 18:13:01 +0300

    first commit object-prototype

--- 2.2-object-prototype/main.js
+++ 2.2-object-prototype/main.js
@@ -1,14 +1,6 @@
-function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
+'use strict'
 
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
-
-    document.getElementById('disclaimer').innerHTML = result;   
-}
-
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-}
+//task 1
 
 function initPrintAnimalSound() {
     const animal = {
@@ -21,8 +13,17 @@ function initPrintAnimalSound() {
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
+    if (animal === undefined) {
+        return null;
+    } else {
+        initPrintAnimalSound.prototype = animal;
+        let sound = animal.sound;
+        return sound;
+    }
+    
 }
+
+//task2
 
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
@@ -35,5 +36,31 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
+    let marksSum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        marksSum += Number(marks[i]);
+    }
+    let roundedAverage = Math.round(marksSum / marks.length);
+    return roundedAverage;
+}
+
+//task 3 
+
+function initCheckBirthday() {
+    const birthday = document.getElementById('birthday').value;
+
+    const result = checkBirthday(birthday) ? "Да" : "Нет";
+
+    document.getElementById('disclaimer').innerHTML = result;   
+}
+
+function checkBirthday(birthday) {
+    let now = Date.now();
+    let diff = now - Number(new Date(birthday));
+    let age = diff / (365.25*24*60*60*1000);
+    if (age < 18) {
+        return false;
+    } else {
+        return true;
+    }
 }
