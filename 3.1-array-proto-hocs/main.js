59c700008f8f59d2cbc13562d26ab1db553fc959
Anna Nosova <anna.nosova.int@gmail.com>
2020-02-25 19:29:14 +0300

    main.js

--- 3.1-array-proto-hocs/main.js
+++ 3.1-array-proto-hocs/main.js
@@ -8,7 +8,7 @@ function sleep(milliseconds)
 
 function sum(...args) {
     // Замедление на половину секунды.
-    sleep(500); 
+    //sleep(500);
     return args.reduce((sum, arg) => {
       return sum += +arg;
     }, 0);
@@ -24,20 +24,65 @@ function compareArrays(arr1, arr2) {
 }
 
 
-/*function memorize(fn, limit) {
-    let results =[];
+function memorize(fn, limit) {
+    let results = [];
 
+    return function() {
+      const args = Array.from(arguments);
+      console.log(`массив: ${args}`);
+
+      let result = results.find(value => { 
+        return compareArrays(value.args, args);
+      });
+
+      if (result) {
+        console.log(`результат вычислений (взят из архива): ${result.result}`);
+        return result.result;
+      } else {
+        console.log(`вычисляю...`);
+        let sum = fn(...args);
+        results.push(
+        {
+            args: args,
+            result: sum,
+        }); 
+        
+        if (results.length > limit) {
+          results = resuls.slice((results.length - limit));
+        }
+
+        console.log(`результат вычислений: ${sum}`);
+        return sum;
+      }
+    }
+}
+
+function testCase(variant) {
+  const testArr = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
+
+  console.time("testCase");
+  for (let i = 7; i > 0; i--) {
+      testArr.forEach(item => variant(...item));
   }
-    args, // массив аргументов, с которыми была вызвана функция
-    result // результат работы
-}*/
+  console.timeEnd("testCase");
+
+}
+
+
+const mSum = memorize(sum, 3);
+
+testCase(mSum);
+//Without sleep(500) - testCase: 20.2119140625ms
+//With sleep(500) - testCase: 1523.958984375ms
+
+testCase(sum);
+//Without sleep(500) -  testCase: 0.158203125ms
+//With sleep(500) - testCase: 17535.8671875ms
 
 
 
-//function testCase {}
-
-console.log(compareArrays([8, 9], [6])); // false, разные значения
-console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
-console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])); // false, разные значения
-console.log(compareArrays([1, 2, 3], [2, 3, 1])); // false, разные индексы, хотя и одинаковые значения
-console.log(compareArrays([8, 1, 2], [8, 1, 2])); // true
+//console.log(compareArrays([8, 9], [6])); // false, разные значения
+//console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
+//console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])); // false, разные значения
+//console.log(compareArrays([1, 2, 3], [2, 3, 1])); // false, разные индексы, хотя и одинаковые значения
+//console.log(compareArrays([8, 1, 2], [8, 1, 2])); // true
